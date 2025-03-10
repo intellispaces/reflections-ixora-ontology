9a7bca3cbde0b5c054bdcd2ae5f0462dafdbf24f
lavor <lavor.andrey@gmail.com>
2025-03-10 10:05:02 +0600

    Rename generated artifact types

--- intellispaces-ixora-data-ontology/src/main/java/tech/intellispaces/jaquarius/ixora/data/association/MapAddOn.java
+++ intellispaces-ixora-data-ontology/src/main/java/tech/intellispaces/jaquarius/ixora/data/association/MapAddOn.java
@@ -3,7 +3,7 @@ package tech.intellispaces.jaquarius.ixora.data.association;
 import tech.intellispaces.ixora.data.association.MapDomain;
 import tech.intellispaces.jaquarius.annotation.Preprocessing;
 
-@Preprocessing(value = MapDomain.class, artifact = "UndefinedPureObject", type = "addon")
+@Preprocessing(value = MapDomain.class, artifact = "UndefinedSimpleObject", type = "addon")
 public interface MapAddOn<K, V> {
 
   java.util.Map<K, V> nativeMap();
--- intellispaces-ixora-data-ontology/src/main/java/tech/intellispaces/jaquarius/ixora/data/collection/CollectionAddOn.java
+++ intellispaces-ixora-data-ontology/src/main/java/tech/intellispaces/jaquarius/ixora/data/collection/CollectionAddOn.java
@@ -3,7 +3,7 @@ package tech.intellispaces.jaquarius.ixora.data.collection;
 import tech.intellispaces.ixora.data.collection.CollectionDomain;
 import tech.intellispaces.jaquarius.annotation.Preprocessing;
 
-@Preprocessing(value = CollectionDomain.class, artifact = "UndefinedPureObject", type = "addon")
+@Preprocessing(value = CollectionDomain.class, artifact = "UndefinedSimpleObject", type = "addon")
 public interface CollectionAddOn<E> {
 
   java.util.Collection<E> nativeCollection();
