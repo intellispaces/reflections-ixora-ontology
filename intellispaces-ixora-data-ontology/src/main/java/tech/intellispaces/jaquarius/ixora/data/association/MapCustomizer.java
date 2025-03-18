82b93e492fb0adc4e22441e903ffe3a6912efdae
lavor <lavor.andrey@gmail.com>
2025-03-18 10:44:41 +0600

    Update collection customizers

--- intellispaces-ixora-data-ontology/src/main/java/tech/intellispaces/jaquarius/ixora/data/association/MapCustomizer.java
+++ intellispaces-ixora-data-ontology/src/main/java/tech/intellispaces/jaquarius/ixora/data/association/MapCustomizer.java
@@ -3,7 +3,7 @@ package tech.intellispaces.jaquarius.ixora.data.association;
 import tech.intellispaces.ixora.data.association.MapDomain;
 import tech.intellispaces.jaquarius.annotation.ArtifactCustomizer;
 
-@ArtifactCustomizer(origin = MapDomain.class, target = "UndefinedSimpleObject")
+@ArtifactCustomizer(origin = MapDomain.class, target = "UndefinedObjectHandle")
 public interface MapCustomizer<K, V> {
 
   java.util.Map<K, V> nativeMap();
--- intellispaces-ixora-data-ontology/src/main/java/tech/intellispaces/jaquarius/ixora/data/collection/CollectionCustomizer.java
+++ intellispaces-ixora-data-ontology/src/main/java/tech/intellispaces/jaquarius/ixora/data/collection/CollectionCustomizer.java
@@ -3,7 +3,7 @@ package tech.intellispaces.jaquarius.ixora.data.collection;
 import tech.intellispaces.ixora.data.collection.CollectionDomain;
 import tech.intellispaces.jaquarius.annotation.ArtifactCustomizer;
 
-@ArtifactCustomizer(origin = CollectionDomain.class, target = "UndefinedSimpleObject")
+@ArtifactCustomizer(origin = CollectionDomain.class, target = "UndefinedObjectHandle")
 public interface CollectionCustomizer<E> {
 
   java.util.Collection<E> nativeCollection();
