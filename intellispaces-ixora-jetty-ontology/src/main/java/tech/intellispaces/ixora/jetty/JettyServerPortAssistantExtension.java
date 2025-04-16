1945cac0bbd88f40d1fbd902baae318c697655ab
lavor <lavor.andrey@gmail.com>
2025-04-16 09:24:33 +0600

    Update JettyServerPortAssistantExtension

--- intellispaces-ixora-jetty-ontology/src/main/java/tech/intellispaces/ixora/jetty/JettyServerPortAssistantExtension.java
+++ intellispaces-ixora-jetty-ontology/src/main/java/tech/intellispaces/ixora/jetty/JettyServerPortAssistantExtension.java
@@ -1,10 +1,15 @@
 package tech.intellispaces.ixora.jetty;
 
-import tech.intellispaces.ixora.http.MovableInboundHttpPortHandle;
 import tech.intellispaces.jaquarius.annotation.AssistantExtension;
+import tech.intellispaces.jaquarius.object.reference.DownwardObjectFactory;
+import tech.intellispaces.jaquarius.object.reference.MovableObjectHandle;
 
 @AssistantExtension(JettyServerPortDomain.class)
 public interface JettyServerPortAssistantExtension {
 
-  MovableJettyServerPortHandle create(int portNumber, MovableInboundHttpPortHandle overlyingPort);
+  MovableJettyServerPortHandle create(int portNumber, MovableObjectHandle<?> overlyingHandle);
+
+  default DownwardObjectFactory<MovableJettyServerPortHandle> factory(int portNumber) {
+    return overlyingHandle -> create(portNumber, overlyingHandle);
+  }
 }
