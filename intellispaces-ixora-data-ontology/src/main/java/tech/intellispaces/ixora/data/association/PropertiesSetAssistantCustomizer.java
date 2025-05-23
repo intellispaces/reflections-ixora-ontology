74f3ee1f6e80ca5b7f22c871f4409c0b3ca15583
Andrey <intellispaces@yahoo.com>
2025-05-23 08:52:07 +0600

    Adds interface NativePropertiesSetPresentable

--- intellispaces-ixora-data-ontology/src/main/java/tech/intellispaces/ixora/data/association/PropertiesSetAssistantCustomizer.java
+++ intellispaces-ixora-data-ontology/src/main/java/tech/intellispaces/ixora/data/association/PropertiesSetAssistantCustomizer.java
@@ -6,4 +6,17 @@ import tech.intellispaces.reflections.framework.annotation.AssistantCustomizer;
 public interface PropertiesSetAssistantCustomizer {
 
   UnmovablePropertiesSetReflection reflectionOf(java.util.Map<String, Object> map);
+
+  /**
+   * Returns a native object representing given reflection.
+   *
+   * @param reflection the reflection.
+   * @return instance of the {@link tech.intellispaces.commons.properties.PropertiesSet}.
+   */
+  default tech.intellispaces.commons.properties.PropertiesSet asNative(PropertiesSetReflection reflection) {
+    if (NativePropertiesSetPresentable.class.isAssignableFrom(reflection.getClass())) {
+      return ((NativePropertiesSetPresentable) reflection).asNativePropertiesSet();
+    }
+    return new PropertiesSetImpl(reflection);
+  }
 }
