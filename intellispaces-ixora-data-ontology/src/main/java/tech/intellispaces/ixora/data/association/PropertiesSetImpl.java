7a096b324eef479cc98814f58959cc2fe9e86afb
alavor <lavor.andrey@gmail.com>
2025-08-14 11:31:30 +0600

    Separate PropertiesSet and TraversablePropertiesSet

--- intellispaces-ixora-data-ontology/src/main/java/tech/intellispaces/ixora/data/association/PropertiesSetImpl.java
+++ intellispaces-ixora-data-ontology/src/main/java/tech/intellispaces/ixora/data/association/PropertiesSetImpl.java
@@ -45,151 +45,22 @@ class PropertiesSetImpl implements PropertiesSet {
   }
 
   @Override
-  public @Nullable Object propertyValue(String name) {
+  public boolean hasValue(String name) {
+    throw NotImplementedExceptions.withCode("CWvkEA");
+  }
+
+  @Override
+  public @Nullable Object getValue(String name) {
     throw NotImplementedExceptions.withCode("9dphudX7");
+  }
+
+  @Override
+  public @Nullable String getString(String name) {
+    throw NotImplementedExceptions.withCode("srafOw");
   }
 
   @Override
   public List<PropertiesSet> subProperties() {
     throw NotImplementedExceptions.withCode("hraVAtcS");
-  }
-
-  @Override
-  public boolean hasTraversePath(String... path) {
-    throw NotImplementedExceptions.withCode("9G1JBgCq");
-  }
-
-  @Override
-  public boolean traverseTargetIsSpecified(String name) {
-    throw NotImplementedExceptions.withCode("beT0Dygs");
-  }
-
-  @Override
-  public boolean traverseTargetIsString(String... path) {
-    throw NotImplementedExceptions.withCode("WKsDJJxt");
-  }
-
-  @Override
-  public boolean traverseTargetIsStringCompatible(String... path) {
-    throw NotImplementedExceptions.withCode("ZzeR2tno");
-  }
-
-  @Override
-  public boolean traverseTargetIsProperties(String... path) {
-    throw NotImplementedExceptions.withCode("OtW44AcR");
-  }
-
-  @Override
-  public boolean traverseTargetIsPropertiesCompatible(String... path) {
-    throw NotImplementedExceptions.withCode("374N4goM");
-  }
-
-  @Override
-  public @Nullable Object traverse(String... path) {
-    throw NotImplementedExceptions.withCode("zS7tO1Ll");
-  }
-
-  @Override
-  public @Nullable Integer traverseToInteger(String... path) {
-    throw NotImplementedExceptions.withCode("TMLH7jip");
-  }
-
-  @Override
-  public @Nullable Integer traverseAndCastToInteger(String... path) {
-    throw NotImplementedExceptions.withCode("nRyKI4DZ");
-  }
-
-  @Override
-  public @Nullable Double traverseToDouble(String... path) {
-    throw NotImplementedExceptions.withCode("uOPXq9Jp");
-  }
-
-  @Override
-  public @Nullable Double traverseAndCastToDouble(String... path) {
-    throw NotImplementedExceptions.withCode("uD3VtADT");
-  }
-
-  @Override
-  public @Nullable String traverseToString(String... path) {
-    throw NotImplementedExceptions.withCode("T1OU8wrJ");
-  }
-
-  @Override
-  public @Nullable String traverseToString(String[] headPath, String... tailPath) {
-    throw NotImplementedExceptions.withCode("vrxoFxKJ");
-  }
-
-  @Override
-  public String traverseToStringOrThrow(String... path) {
-    throw NotImplementedExceptions.withCode("mnK4Sfqr");
-  }
-
-  @Override
-  public @Nullable String traverseAndCastToString(String... path) {
-    throw NotImplementedExceptions.withCode("3usXpBbq");
-  }
-
-  @Override
-  public @Nullable PropertiesSet traverseToProperties(String... path) {
-    throw NotImplementedExceptions.withCode("PjOGBgio");
-  }
-
-  @Override
-  public PropertiesSet traverseToPropertiesOrThrow(String... path) {
-    throw NotImplementedExceptions.withCode("3y0vZA9h");
-  }
-
-  @Override
-  public @Nullable PropertiesSet traverseAndCastToProperties(String... path) {
-    throw NotImplementedExceptions.withCode("bGZut18Z");
-  }
-
-  @Override
-  public @Nullable List<Integer> traverseToIntegerList(String... path) {
-    throw NotImplementedExceptions.withCode("TpY81jvs");
-  }
-
-  @Override
-  public @Nullable List<Integer> traverseAndCastToIntegerList(String... path) {
-    throw NotImplementedExceptions.withCode("zuXJbJJ6");
-  }
-
-  @Override
-  public @Nullable List<Double> traverseToDoubleList(String... path) {
-    throw NotImplementedExceptions.withCode("ALhZf3PY");
-  }
-
-  @Override
-  public @Nullable List<Double> traverseAndCastToDoubleList(String... path) {
-    throw NotImplementedExceptions.withCode("Bqy5auTW");
-  }
-
-  @Override
-  public @Nullable List<String> traverseToStringList(String... path) {
-    throw NotImplementedExceptions.withCode("ydWCWzUc");
-  }
-
-  @Override
-  public @Nullable List<String> traverseAndCastToStringList(String... path) {
-    throw NotImplementedExceptions.withCode("ljuezNZF");
-  }
-
-  @Override
-  public @Nullable List<PropertiesSet> traverseToPropertiesList(String... path) {
-    throw NotImplementedExceptions.withCode("SyqO9VMt");
-  }
-
-  @Override
-  public @Nullable List<PropertiesSet> traverseToPropertiesList(
-      String[] headPath, String... tailPath
-  ) {
-    throw NotImplementedExceptions.withCode("49yaFNcu");
-  }
-
-  @Override
-  public @Nullable List<PropertiesSet> traverseAndCastToPropertiesList(
-      String... path
-  ) {
-    throw NotImplementedExceptions.withCode("XvxvAjkm");
   }
 }
